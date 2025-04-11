9a411724449098b94a1fcbc89c6b5f94a0ffdf8a
LAB_J205 <labj205@continental.edu.pe>
2025-04-11 13:19:04 -0500

    Divisores

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 Este script en Python permite sumar dos números, ya sean enteros (`int`) o flotantes (`float`). Antes de realizar la operación, valida que ambos operandos sean numéricos. En caso contrario, muestra un mensaje de error indicando que la entrada no es válida.
 
 ## Desarrollador
-| Apellidos y nombres |
-| ---- |
-| Daniel Gamarra Moreno |
+| Apellidos y nombres        |
+|----------------------------|
+| Urquizo Oré Francis Maxuel |
 ## Requisitos previos
 
 - Python 3.x instalado en el sistema.
--- test_suma.py
+++ test_suma.py
@@ -1,5 +1,5 @@
 import unittest
-from suma import Suma
+from operaciones_aritmeticas import OperacionesAritmeticas
 
 class TestSuma(unittest.TestCase):
     def test_suma_dosNumeros_retornaSuma(self):
@@ -9,7 +9,7 @@ class TestSuma(unittest.TestCase):
 
         resultadoEsperado = 25
 
-        objSuma = Suma(operando1, operando2)
+        objSuma = OperacionesAritmeticas(operando1, operando2)
 
         # Act
         resultadoActual = objSuma.calcularSuma()
@@ -18,10 +18,32 @@ class TestSuma(unittest.TestCase):
         self.assertEqual(resultadoEsperado, resultadoActual, " Fallo la suma")
 
     def test_suma_operadorNoNumerico_lanzaExcepcion(self):
-        objSuma = Suma(3, "a")
+        objSuma = OperacionesAritmeticas(3, "a")
         with self.assertRaises(TypeError):
             objSuma.calcularSuma()
 
+    def test_division_dosNumeros_retornaDivision(self):
+        # Arrange
+        dividendo = 10
+        divisor = 15
 
+        resultadoEsperado = 0.666
+
+        objSuma = OperacionesAritmeticas(dividendo, divisor)
+
+        # Act
+        resultadoActual = objSuma.calcular_division()
+
+        # Assert
+        self.assertAlmostEqual(resultadoEsperado, resultadoActual, 2," Fallo la división")
+    def test_division_operadorNoNumerico_lanzaExcepcion(self):
+        objSuma = OperacionesAritmeticas(3, "a")
+        with self.assertRaises(TypeError):
+            objSuma.calcular_division()
+
+    def test_division_divisorCero_lanzaExcepcion(self):
+        objSuma = OperacionesAritmeticas(3, 0)
+        with self.assertRaises(ZeroDivisionError):
+            objSuma.calcular_division()
 if __name__ == '__main__':
     unittest.main()
